a31162179c7de7f7a77a83581c1c33e4466b97c8
Candra Wijaya <candradenis2317@gmail.com>
2025-04-04 18:42:46 +0700

    key ring fix

--- Entities/ChatChat.cs
+++ Entities/ChatChat.cs
@@ -6,14 +6,18 @@ namespace Entities;
 public partial class ChatChat
 {
     public Guid Id { get; set; }
+
     public Guid UserId { get; set; }
+
     public Guid SellerId { get; set; }
+
     public string? LastMessage { get; set; }
+
     public DateTime? UpdatedAt { get; set; }
 
-    public virtual ChatUser Seller { get; set; } = null!;
-    public virtual ChatUser User { get; set; } = null!;
     public virtual ICollection<ChatMessage> ChatMessages { get; set; } = new List<ChatMessage>();
 
-}
+    public virtual ChatUser Seller { get; set; } = null!;
 
+    public virtual ChatUser User { get; set; } = null!;
+}
